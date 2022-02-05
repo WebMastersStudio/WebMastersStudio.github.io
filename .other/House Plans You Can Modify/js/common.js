0a3e342f660abd31d0534945b8b17680c6397a57
WebMastersStudio <41670942+WebMastersStudio@users.noreply.github.com>
2022-02-05 14:41:10 +0200

    add house plans

--- .other/House Plans You Can Modify/js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".navbar").click(function(){
-    $(".nav-page").addClass("nav-page-active");
-  });
-  $(".close").click(function(){
-    $(".nav-page").removeClass("nav-page-active");
-  });
-})
