9148c8d0046f2b247d6fbd08fa7f1c9d14ac1dad
WebMastersStudio <41670942+WebMastersStudio@users.noreply.github.com>
2022-02-12 17:30:18 +0200

    up

--- HausePlans/js/common.js
+++ HausePlans/js/common.js
@@ -120,7 +120,7 @@ $(document).ready(function(){
   var target = $('.footer-empty-place');
   var targetPos = target.offset().top;
   var winHeight = $(window).height();
-  var scrollToElem = targetPos - winHeight;
+  var scrollToElem = targetPos;
   $(window).scroll(function(){
     var winScrollTop = $(this).scrollTop();
     if(winScrollTop > scrollToElem){
